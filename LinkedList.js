a2e9712019af4f195953a63bd79d39577cf2f6fd
ezQel <ezekiel3499@gmail.com>
2020-03-06 14:13:47 +0300

    created Liked list class with insert method

--- LinkedList.js
+++ LinkedList.js
@@ -5,3 +5,28 @@ class Node{
         this.data = name;
     }
 }
+
+class LinkedList{
+    constructor(){
+        this.head = new Node("head");
+    }
+    
+    insert(item){ // inserts a new item to the end of the linked list
+        let newNode = new Node(item);
+        let curr = this.head;
+
+        while(curr.next != null){
+            curr = curr.next;
+        }
+
+        newNode.next = null;
+        newNode.previous = curr;
+        curr.next = newNode;  
+    }
+}
+
+let llist =  new LinkedList();
+llist.insert('Mombasa');
+llist.insert('Nairobi');
+llist.insert('Kisumu');
+console.log(llist);
