2bdf047d6d19b37fd3bf95cfe74ab317932b785d
ezQel <ezekiel3499@gmail.com>
2020-03-09 22:12:53 +0300

    BST class

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,7 +1,48 @@
 class Node{
-    constructor(data){
-        this.data = data;
+    constructor(value){
+        this.value = value;
         this.left = null;
         this.right = null;
     }
+}
+
+class BST{
+    constructor(){
+        this.head = null;
+    }
+    
+    insert(value){
+        let node = new Node(value);
+        let curr = this.head;
+
+        if (this.head == null) {
+            this.head = node;
+        }
+        else {
+            while(true){
+                if(value > curr.value){
+                    if(curr.right == null){
+                        curr.right = node;
+                        break;
+                    }
+                    else{
+                        curr = curr.right;
+                    }
+                }
+                else {
+                    if(curr.left == null){
+                        curr.left = node;
+                        break;
+                    }
+                    else{
+                        curr = curr.left;
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+
+    
 }
